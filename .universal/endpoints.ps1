17994fda64808f8a436164d89a1d6b2cbf430659
admin <admin>
2026-01-14 13:29:42 -0600

    Remove integrated switch.

--- .universal/endpoints.ps1
+++ .universal/endpoints.ps1
@@ -1,5 +1,5 @@
 ﻿New-PSUEndpoint -Url "/api/user" -Method @('POST') -Endpoint {
     param($UserName, $FirstName, $LastName)
 
-    Invoke-PSUScript -Name "NewUserAccount.ps1" -FirstName $FirstName -LastName $LastName -UserName $UserName -Wait -Integrated
+    Invoke-PSUScript -Name "NewUserAccount.ps1" -FirstName $FirstName -LastName $LastName -UserName $UserName -Wait
 } -Authentication -Role @('Administrator')
